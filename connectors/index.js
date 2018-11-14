4a435b8d05ded145f6d375b8c9a89adb21aba097
Pavel Romanov <pv_romanov@bingo-boom.com>
2018-11-14 15:50:29 +0300

    [FIX] Connector factory

--- connectors/index.js
+++ connectors/index.js
@@ -1,7 +1,9 @@
 const PostgresConnector = require('./postgres-connector');
+const MysqlConnector = require('./mysql-connector');
 const TestingConnector = require('./testing-connector');
 
 module.exports = {
     PostgresConnector,
     TestingConnector,
+    MysqlConnector,
 };
